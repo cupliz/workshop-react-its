c9a1e776d44e58028f988cffe8a65cf1f103aa02
iyan <iyan.cupliz@gmail.com>
2022-03-09 16:12:10 +0700

    API CRUD

--- src/Coba2.js
+++ src/Coba2.js
@@ -1,40 +1,49 @@
 import React, { useEffect, useState } from 'react'
 
-function Coba4({ second }) {
+function Coba4({ second, ganti }) {
   const [state, setstate] = useState(1)
   // console.log('render coba4')
 
+  // useEffect(() => {
+  //   console.log('componentDidMount')
+  //   alert("komponen 4 ditampilkan")
+  //   return () => {
+  //     console.log('componentWillUnmount')
+  //     alert("komponen 4 disembunyikan")
+  //   }
+  // }, [])
   useEffect(() => {
-    console.log('componentDidMount')
-    alert("komponen 4 ditampilkan")
-    return () => {
-      console.log('componentWillUnmount')
-      alert("komponen 4 disembunyikan")
-    }
-  }, [])
-  useEffect(()=>{
     console.log('componentDidUpdate')
-  },[state])
+  }, [state])
   return (
     <div>
       <button onClick={() => {
         setstate(state + 1)
-        // gantiStateFirst()
+        ganti(second + 1)
       }}>Click 4</button>
-      <div>Coba4 : {state}</div>
+      {/* <div>Coba4 : {state}</div> */}
+      <br />
+      <Coba3 isi={second} />
     </div>
   )
 }
 function Coba3({ isi }) {
   // console.log('render coba3', isi)
   return (
-    <div>Jumlah {isi}</div>
+    <div>Jumlah {isi}
+      <Coba5 data={isi} />
+    </div>
+  )
+}
+const Coba5 = ({ data }) => {
+  return (
+    <div>Coba 5: {data}</div>
   )
 }
 export default function Coba2({ data, tampil }) {
   const [first, setfirst] = useState(1)
   const [second, setsecond] = useState(1)
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(true)
 
   useEffect(() => {
     // console.log('show me')
@@ -45,10 +54,10 @@ export default function Coba2({ data, tampil }) {
       <button onClick={() => {
         setfirst(first + 1)
       }}>Click 1</button>
+      <br />
       <button onClick={() => {
         setsecond(second + 1)
       }}>Click 2</button>
-      <Coba3 isi={first} />
       <br />
       Click 2 value: {second}
       <br />
@@ -56,7 +65,7 @@ export default function Coba2({ data, tampil }) {
         onClick={() => setShow(!show)}>
         Show/Hide
       </button>
-      {show && <Coba4 second={second} />}
+      {show && <Coba4 second={second} ganti={setsecond} />}
 
     </div>
   )
--- src/FormAPI.js
+++ src/FormAPI.js
@@ -3,23 +3,51 @@ import axios from 'axios'
 
 export default function FormAPI() {
   const [data, setdata] = useState([])
+  const [edit, setedit] = useState(null)
   const getData = () => {
+    console.log('get data')
     axios.get('http://localhost:3001/cars')
       .then(hasil => {
         setdata(hasil.data)
       })
   }
-  useEffect(() => {
-    getData()
-  }, [])
   const handleSubmit = (e) => {
     e.preventDefault()
     const value = e.target.car.value
     axios.post('http://localhost:3001/cars', { name: value })
-    getData()
+      .then(() => {
+        console.log('post')
+        getData()
+      })
+
     e.target.car.value = ''
   }
-  // console.log(data)
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:3001/cars/${id}`).then(() => {
+      console.log('delete')
+      getData()
+    })
+  }
+  // const handleEdit = (e, id) => {
+  //   e.preventDefault()
+  //   axios.patch(`http://localhost:3001/cars/${id}`, { name: e.target.car.value })
+  //     .then(() => { 
+  //       getData() 
+  //       setedit(null)
+  //     })
+  // }
+  const handleEdit = (e) => {
+    e.preventDefault()
+    console.log('index edit', edit, data[edit].id)
+    axios.patch(`http://localhost:3001/cars/${data[edit].id}`, { name: e.target.car.value })
+      .then(() => { 
+        getData() 
+        setedit(null)
+      })
+  }
+  useEffect(() => {
+    getData()
+  }, [])
   return (
     <div className='container'>
       FormAPI
@@ -28,8 +56,20 @@ export default function FormAPI() {
         <button type="submit">Save</button>
       </form>
       {data.map((car, i) => {
-        return <li key={i}>{car.name}</li>
+        return <li key={i} className='row'>
+          {edit === i ?
+            <form className='col' onSubmit={(event)=>handleEdit(event)}>
+              <input name="car" defaultValue={car.name} />
+              <button>Save</button>
+            </form>
+            : car.name
+          }
+          <div className='col row'>
+            <div className="col" onClick={() => setedit(i)}>edit</div>
+            <div className="col" onClick={() => handleDelete(car.id)}>delete</div>
+          </div>
+        </li>
       })}
-    </div>
+    </div >
   )
 }
